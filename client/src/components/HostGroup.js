6283696efd6f70168706baa83ab450c8d5d3ed15
triciahughes <hughes.a.tricia@gmail.com>
2023-03-29 00:59:44 -0700

    refactor css and add columns

--- client/src/components/HostGroup.js
+++ client/src/components/HostGroup.js
@@ -1,51 +1,53 @@
 import MemberPanel from "./MemberPanel";
 import BookPanel from "./BookPanel";
 import DiscussionPanel from "./DiscussionPanel";
-import { useState} from "react";
-import { useParams } from 'react-router-dom'
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 function HostGroup() {
+  const params = useParams();
+  console.log(params);
 
-    const params = useParams()
-    console.log(params)
+  const groupDetails = {
+    host_id: 15,
+    id: 11,
+    member_details: [
+      {
+        email: "hughes.a.tricia@gmail.com",
+        first_name: "Tricia",
+        id: 14,
+        last_name: "Hughes",
+      },
+    ],
+    name: "Book Bees",
+  };
 
-    const groupDetails = {
-        "host_id": 15,
-        "id": 11,
-        "member_details": [
-        {
-        "email": "hughes.a.tricia@gmail.com",
-        "first_name": "Tricia",
-        "id": 14,
-        "last_name": "Hughes"
-        }
-        ],
-        "name": "Book Bees"
-        }
+  const bookDetails = {
+    id: 6,
+    group_id: 12,
+    title: "The Hitchhiker's Guide to the Galaxy",
+    author: "Douglas Adams",
+    image:
+      "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51+qSD6UwfL._SX312_BO1,204,203,200_.jpg",
+    publication_year: 1979,
+    genre: "Science Fiction",
+    votes: 5,
+    featured: true,
+  };
 
-    const bookDetails = {
-        id: 6,
-        group_id: 12,
-        title: "The Hitchhiker's Guide to the Galaxy",
-        author: "Douglas Adams",
-        image: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51+qSD6UwfL._SX312_BO1,204,203,200_.jpg",
-        publication_year: 1979,
-        genre: "Science Fiction",
-        votes: 5,
-        featured: true
-    }
+  const [selectedGroup, setSelectedGroup] = useState(groupDetails);
+  const [featuredBook, setFeaturedBook] = useState(bookDetails);
 
-    const [selectedGroup, setSelectedGroup] = useState(groupDetails)
-    const [featuredBook, setFeaturedBook] = useState(bookDetails)
-
-    return(
+  return (
     <>
-    <h1>{selectedGroup.name}</h1>
-    <MemberPanel members={selectedGroup.member_details}/>
-    <BookPanel book={featuredBook}/>
-    <DiscussionPanel />
+      <h1>{selectedGroup.name}</h1>
+      <div className="hostPanels">
+        <MemberPanel members={selectedGroup.member_details} />
+        <BookPanel book={featuredBook} />
+        <DiscussionPanel />
+      </div>
     </>
-    ) 
+  );
 }
 
-export default HostGroup
+export default HostGroup;
