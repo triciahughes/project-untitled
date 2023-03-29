c9ed04f156123b1636d84c1d82fd639fe3783ef8
Bianca Aspin <bianca@mj94.net>
2023-03-29 09:16:39 -0700

    Group data now populating and being passed down to child components.

--- client/src/components/HostGroup.js
+++ client/src/components/HostGroup.js
@@ -9,51 +9,34 @@ function HostGroup() {
   console.log(params)
   const groupId = params['groupId']
 
-  
-
-  const groupDetails = {
-    host_id: 15,
-    id: 11,
-    member_details: [
-      {
-        email: "hughes.a.tricia@gmail.com",
-        first_name: "Tricia",
-        id: 14,
-        last_name: "Hughes",
-      },
-    ],
-    name: "Book Bees",
-  };
-
-  const bookDetails = {
-    id: 6,
-    group_id: 12,
-    title: "The Hitchhiker's Guide to the Galaxy",
-    author: "Douglas Adams",
-    image:
-      "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/51+qSD6UwfL._SX312_BO1,204,203,200_.jpg",
-    publication_year: 1979,
-    genre: "Science Fiction",
-    votes: 5,
-    featured: true,
-  };
-
   const [selectedGroup, setSelectedGroup] = useState([]);
+  const [members, setMembers] = useState([])
   const [featuredBook, setFeaturedBook] = useState([]);
 
   useEffect( () => {
+    
+    function setMembersandBooks(data) {
+        const memberArray = [...data.member_details]
+        setMembers(memberArray)
+
+        const book = data.books[0]
+        setFeaturedBook(book)
+    }
+    
+    
     fetch(`/host_group/${groupId}`)
         .then(res => res.json())
-        .then(groupData => setSelectedGroup(groupData) && setFeaturedBook(groupData.books[0]))
+        .then(groupData => {
+            setSelectedGroup(groupData);
+            setMembersandBooks(groupData)
+        })
     }, [groupId])
 
   return (
     <>
       <h1>{selectedGroup.name}</h1>
       <div className="hostPanels">
-        { (selectedGroup) 
-        ? <MemberPanel members={groupDetails.member_details} />
-        : null}
+        <MemberPanel members={members} />
         <BookPanel book={featuredBook} />
         <DiscussionPanel />
       </div>
