b4549f9bd53d793e6500a2cfeb6850cbdf08c883
triciahughes <hughes.a.tricia@gmail.com>
2023-03-30 10:19:02 -0700

    members only comment feature started

--- client/src/components/MemberGroup.js
+++ client/src/components/MemberGroup.js
@@ -4,7 +4,7 @@ import DiscussionPanel from "./DiscussionPanel";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-function MemberGroup() {
+function MemberGroup({ user }) {
   const params = useParams();
   const groupId = params["groupId"];
 
@@ -43,7 +43,7 @@ function MemberGroup() {
       <div className="hostPanels">
         <MemberOnlyPanel members={members} />
         <BookPanel book={featuredBook} />
-        <DiscussionPanel book={featuredBook} />
+        <DiscussionPanel book={featuredBook} user={user} />
       </div>
     </>
   );
