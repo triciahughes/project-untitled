5e39560b7c26e28eef6851b08b3eb789f942d98e
Bianca Aspin <bianca@mj94.net>
2023-03-31 08:34:58 -0700

    Fix for page crash upon loading new group.

--- client/src/components/BookPanel.js
+++ client/src/components/BookPanel.js
@@ -1,5 +1,23 @@
 import currentlyReading from "../currently.png";
 function BookPanel({ book }) {
+  
+  if (!book || book.length === 0) {
+
+    return(
+      <section id="bookPanel" className="panel">
+      <img
+        alt="Currently Reading"
+        src={currentlyReading}
+        className="currentlyReading"
+      ></img>
+      <h4 className="bookPanel">
+        You need to select a book for the group to read.
+      </h4>
+    </section>
+    )
+  }
+  
+  
   return (
     <section id="bookPanel" className="panel">
       <img
@@ -7,7 +25,6 @@ function BookPanel({ book }) {
         src={currentlyReading}
         className="currentlyReading"
       ></img>
-      {/* {book ? :} */}
       <h4 className="bookPanel">
         <em>{book.title}</em>
         <br />
